a28f135f07c9c4c955a9de2c50ba1f5594f98795
Alvin John <92667829+ajiwnl@users.noreply.github.com>
2025-01-31 17:19:19 +0800

    Add files via upload

---
+++ Rectangle/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Rectangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LengthIn = new System.Windows.Forms.TextBox();
+            this.WidthIn = new System.Windows.Forms.TextBox();
+            this.HeightIn = new System.Windows.Forms.TextBox();
+            this.DisplayBtn = new System.Windows.Forms.Button();
+            this.RectangleRadBtn = new System.Windows.Forms.RadioButton();
+            this.BoxRadBtn = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(215, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height";
+            // 
+            // LengthIn
+            // 
+            this.LengthIn.Location = new System.Drawing.Point(290, 57);
+            this.LengthIn.Margin = new System.Windows.Forms.Padding(5);
+            this.LengthIn.Name = "LengthIn";
+            this.LengthIn.Size = new System.Drawing.Size(164, 26);
+            this.LengthIn.TabIndex = 3;
+            // 
+            // WidthIn
+            // 
+            this.WidthIn.Location = new System.Drawing.Point(290, 93);
+            this.WidthIn.Margin = new System.Windows.Forms.Padding(5);
+            this.WidthIn.Name = "WidthIn";
+            this.WidthIn.Size = new System.Drawing.Size(164, 26);
+            this.WidthIn.TabIndex = 4;
+            // 
+            // HeightIn
+            // 
+            this.HeightIn.Location = new System.Drawing.Point(290, 129);
+            this.HeightIn.Margin = new System.Windows.Forms.Padding(5);
+            this.HeightIn.Name = "HeightIn";
+            this.HeightIn.Size = new System.Drawing.Size(164, 26);
+            this.HeightIn.TabIndex = 5;
+            // 
+            // DisplayBtn
+            // 
+            this.DisplayBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayBtn.Location = new System.Drawing.Point(264, 249);
+            this.DisplayBtn.Margin = new System.Windows.Forms.Padding(5);
+            this.DisplayBtn.Name = "DisplayBtn";
+            this.DisplayBtn.Size = new System.Drawing.Size(137, 42);
+            this.DisplayBtn.TabIndex = 6;
+            this.DisplayBtn.Text = "Display";
+            this.DisplayBtn.UseVisualStyleBackColor = true;
+            this.DisplayBtn.Click += new System.EventHandler(this.DisplayBtn_Click);
+            // 
+            // RectangleRadBtn
+            // 
+            this.RectangleRadBtn.AutoSize = true;
+            this.RectangleRadBtn.Location = new System.Drawing.Point(264, 178);
+            this.RectangleRadBtn.Name = "RectangleRadBtn";
+            this.RectangleRadBtn.Size = new System.Drawing.Size(189, 24);
+            this.RectangleRadBtn.TabIndex = 7;
+            this.RectangleRadBtn.TabStop = true;
+            this.RectangleRadBtn.Text = "Calculate Rectangle";
+            this.RectangleRadBtn.UseVisualStyleBackColor = true;
+            // 
+            // BoxRadBtn
+            // 
+            this.BoxRadBtn.AutoSize = true;
+            this.BoxRadBtn.Location = new System.Drawing.Point(264, 217);
+            this.BoxRadBtn.Name = "BoxRadBtn";
+            this.BoxRadBtn.Size = new System.Drawing.Size(137, 24);
+            this.BoxRadBtn.TabIndex = 8;
+            this.BoxRadBtn.TabStop = true;
+            this.BoxRadBtn.Text = "Calculate Box";
+            this.BoxRadBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 382);
+            this.Controls.Add(this.BoxRadBtn);
+            this.Controls.Add(this.RectangleRadBtn);
+            this.Controls.Add(this.DisplayBtn);
+            this.Controls.Add(this.HeightIn);
+            this.Controls.Add(this.WidthIn);
+            this.Controls.Add(this.LengthIn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LengthIn;
+        private System.Windows.Forms.TextBox WidthIn;
+        private System.Windows.Forms.TextBox HeightIn;
+        private System.Windows.Forms.Button DisplayBtn;
+        private System.Windows.Forms.RadioButton RectangleRadBtn;
+        private System.Windows.Forms.RadioButton BoxRadBtn;
+    }
+}
+
---
+++ Rectangle/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Rectangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayBtn_Click(object sender, EventArgs e)
+        {
+            //input
+            int len, wid, height;
+            Input(out len, out wid, out height);
+
+            //instantiation
+            ParentRectangle r1 = new ParentRectangle();
+            Box boxcon = BoxMethod (len, wid, height);
+
+
+            if (RectangleRadBtn.Checked == true) {
+                Rectangle(r1, len, wid); //process
+                DisplayRectangle(r1); //output
+
+            }
+
+            if (BoxRadBtn.Checked == true) {
+                BoxMethod(len, wid, height);
+                DisplayBox(boxcon);
+
+            }
+        }
+
+        private static void DisplayBox(Box boxcon)
+        {
+            MessageBox.Show(boxcon.ToString());
+        }
+
+        private static void DisplayRectangle(ParentRectangle r1)
+        {
+            MessageBox.Show(r1.ToString());
+        }
+
+        private Box BoxMethod(int len, int wid, int height)
+        {
+            Box boxcon = new Box(len, wid, height);
+            return boxcon;
+        }
+
+        private static void Rectangle(ParentRectangle r1, int len, int wid)
+        {
+            r1.Width = wid;
+            r1.Length = len;
+        }
+
+        private void Input(out int len, out int wid, out int height)
+        {
+            len = Int32.Parse(LengthIn.Text);
+            wid = Int32.Parse(WidthIn.Text);
+            height = Int32.Parse(HeightIn.Text);
+        }
+    }
+}
