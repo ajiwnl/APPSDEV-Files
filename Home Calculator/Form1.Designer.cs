6b11bca5ea85686c2a363abed9b329e1f19131e2
Alvin John <92667829+ajiwnl@users.noreply.github.com>
2025-01-31 14:39:18 +0800

    Add files via upload

---
+++ Home Calculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Financial_Calculator
+{
+    partial class FinancialCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HomePriceTxtBox = new System.Windows.Forms.TextBox();
+            this.InterestRateTxtBox = new System.Windows.Forms.TextBox();
+            this.YrsToPayTxtBox = new System.Windows.Forms.TextBox();
+            this.PercentDPTxtBox = new System.Windows.Forms.TextBox();
+            this.OutputListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Home Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Interest Rate (%)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "No. of Years to Pay";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Percent as Down Payment";
+            // 
+            // HomePriceTxtBox
+            // 
+            this.HomePriceTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomePriceTxtBox.Location = new System.Drawing.Point(119, 14);
+            this.HomePriceTxtBox.Name = "HomePriceTxtBox";
+            this.HomePriceTxtBox.Size = new System.Drawing.Size(152, 26);
+            this.HomePriceTxtBox.TabIndex = 4;
+            // 
+            // InterestRateTxtBox
+            // 
+            this.InterestRateTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InterestRateTxtBox.Location = new System.Drawing.Point(159, 55);
+            this.InterestRateTxtBox.Name = "InterestRateTxtBox";
+            this.InterestRateTxtBox.Size = new System.Drawing.Size(152, 26);
+            this.InterestRateTxtBox.TabIndex = 5;
+            // 
+            // YrsToPayTxtBox
+            // 
+            this.YrsToPayTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YrsToPayTxtBox.Location = new System.Drawing.Point(173, 99);
+            this.YrsToPayTxtBox.Name = "YrsToPayTxtBox";
+            this.YrsToPayTxtBox.Size = new System.Drawing.Size(152, 26);
+            this.YrsToPayTxtBox.TabIndex = 6;
+            // 
+            // PercentDPTxtBox
+            // 
+            this.PercentDPTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PercentDPTxtBox.Location = new System.Drawing.Point(224, 146);
+            this.PercentDPTxtBox.Name = "PercentDPTxtBox";
+            this.PercentDPTxtBox.Size = new System.Drawing.Size(152, 26);
+            this.PercentDPTxtBox.TabIndex = 7;
+            // 
+            // OutputListView
+            // 
+            this.OutputListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.OutputListView.FullRowSelect = true;
+            this.OutputListView.GridLines = true;
+            this.OutputListView.HideSelection = false;
+            this.OutputListView.Location = new System.Drawing.Point(89, 234);
+            this.OutputListView.Name = "OutputListView";
+            this.OutputListView.Size = new System.Drawing.Size(407, 174);
+            this.OutputListView.TabIndex = 8;
+            this.OutputListView.UseCompatibleStateImageBehavior = false;
+            this.OutputListView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Down Payment";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Amount Financed";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Monthly Payment";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Total Interest";
+            this.columnHeader4.Width = 100;
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.Location = new System.Drawing.Point(30, 191);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(137, 37);
+            this.CalculateBtn.TabIndex = 9;
+            this.CalculateBtn.Text = "CALCULATE";
+            this.CalculateBtn.UseVisualStyleBackColor = true;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(229, 191);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(137, 37);
+            this.ClearBtn.TabIndex = 10;
+            this.ClearBtn.Text = "CLEAR";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // FinancialCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 447);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.CalculateBtn);
+            this.Controls.Add(this.OutputListView);
+            this.Controls.Add(this.PercentDPTxtBox);
+            this.Controls.Add(this.YrsToPayTxtBox);
+            this.Controls.Add(this.InterestRateTxtBox);
+            this.Controls.Add(this.HomePriceTxtBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FinancialCalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Financial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox HomePriceTxtBox;
+        private System.Windows.Forms.TextBox InterestRateTxtBox;
+        private System.Windows.Forms.TextBox YrsToPayTxtBox;
+        private System.Windows.Forms.TextBox PercentDPTxtBox;
+        private System.Windows.Forms.ListView OutputListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Button ClearBtn;
+    }
+}
+
---
+++ RecordingStudioApp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecordingStudioApp
+{
+    public partial class RecordingStudioRentalForm : Form
+    {
+        public RecordingStudioRentalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CurrentBand(object sender, EventArgs e)
+        {
+            CurrentBandLbl.Text = BandNametxtbox.Text;
+        }
+
+        const double MINTOHR = 60.0;
+        const int RATE = 200;
+        byte count = 0;
+        int MinutesIN;
+        double currentCharge = 0, totalCharge = 0, avg = 0;
+
+
+        private void CurrentCharge(object sender, EventArgs e)
+        {
+            try {
+               MinutesIN = Convert.ToInt32(RentalMintxtbox.Text);
+                currentCharge = (MinutesIN / MINTOHR) * RATE;
+                CurrentChargeLbl.Text = String.Format(CultureInfo.GetCultureInfo("en-PH"),
+                                 "{0:c}", currentCharge);
+            }
+            catch(Exception ex) {
+                
+            }
+        }
+
+        private void CalculateBtn_Click(object sender, EventArgs e)
+        {
+            count++;
+            totalCharge += currentCharge;
+            avg = totalCharge/ count;
+
+            TotalBandLbl.Text = count.ToString();
+            TotalChargeLbl.Text = String.Format(CultureInfo.GetCultureInfo("en-PH"),
+                                 "{0:c}", totalCharge);
+            AvgChargeLbl.Text = String.Format(CultureInfo.GetCultureInfo("en-PH"),
+                                 "{0:c}", avg);
+        }
+
+    }
+}
