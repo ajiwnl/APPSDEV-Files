905048c0e077961b7c63acdba91c0b6833f35029
Alvin John <92667829+ajiwnl@users.noreply.github.com>
2025-01-31 17:13:03 +0800

    Add files via upload

---
+++ Bagel Menu/Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace Bagel_Menu
+{
+    partial class BakeryBagelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SodaCBox = new System.Windows.Forms.CheckBox();
+            this.SalCbox = new System.Windows.Forms.CheckBox();
+            this.BagCbox = new System.Windows.Forms.CheckBox();
+            this.BagelGBox = new System.Windows.Forms.GroupBox();
+            this.CCCbox = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.LoxCbox = new System.Windows.Forms.CheckBox();
+            this.SaladGBox = new System.Windows.Forms.GroupBox();
+            this.ChefRadBtn = new System.Windows.Forms.RadioButton();
+            this.SpinRadBtn = new System.Windows.Forms.RadioButton();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.SodaGBox = new System.Windows.Forms.GroupBox();
+            this.CSRadBtn = new System.Windows.Forms.RadioButton();
+            this.ColaRadBtn = new System.Windows.Forms.RadioButton();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bagelPrice = new System.Windows.Forms.Label();
+            this.saladPrice = new System.Windows.Forms.Label();
+            this.sodaPrice = new System.Windows.Forms.Label();
+            this.TotalBtn = new System.Windows.Forms.Button();
+            this.totalPrice = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.BagelGBox.SuspendLayout();
+            this.SaladGBox.SuspendLayout();
+            this.SodaGBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.SodaCBox);
+            this.groupBox1.Controls.Add(this.SalCbox);
+            this.groupBox1.Controls.Add(this.BagCbox);
+            this.groupBox1.Location = new System.Drawing.Point(20, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(231, 302);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FOOD AND BEVERAGES";
+            // 
+            // SodaCBox
+            // 
+            this.SodaCBox.AutoSize = true;
+            this.SodaCBox.Location = new System.Drawing.Point(22, 149);
+            this.SodaCBox.Name = "SodaCBox";
+            this.SodaCBox.Size = new System.Drawing.Size(70, 24);
+            this.SodaCBox.TabIndex = 3;
+            this.SodaCBox.Text = "Soda";
+            this.SodaCBox.UseVisualStyleBackColor = true;
+            this.SodaCBox.CheckedChanged += new System.EventHandler(this.SodaCBox_CheckedChanged);
+            // 
+            // SalCbox
+            // 
+            this.SalCbox.AutoSize = true;
+            this.SalCbox.Location = new System.Drawing.Point(22, 94);
+            this.SalCbox.Name = "SalCbox";
+            this.SalCbox.Size = new System.Drawing.Size(74, 24);
+            this.SalCbox.TabIndex = 2;
+            this.SalCbox.Text = "Salad";
+            this.SalCbox.UseVisualStyleBackColor = true;
+            this.SalCbox.CheckedChanged += new System.EventHandler(this.SalCbox_CheckedChanged);
+            // 
+            // BagCbox
+            // 
+            this.BagCbox.AutoSize = true;
+            this.BagCbox.Location = new System.Drawing.Point(22, 39);
+            this.BagCbox.Name = "BagCbox";
+            this.BagCbox.Size = new System.Drawing.Size(74, 24);
+            this.BagCbox.TabIndex = 1;
+            this.BagCbox.Text = "Bagel";
+            this.BagCbox.UseVisualStyleBackColor = true;
+            this.BagCbox.CheckedChanged += new System.EventHandler(this.BagCbox_CheckedChanged);
+            // 
+            // BagelGBox
+            // 
+            this.BagelGBox.Controls.Add(this.CCCbox);
+            this.BagelGBox.Controls.Add(this.checkBox4);
+            this.BagelGBox.Controls.Add(this.checkBox5);
+            this.BagelGBox.Controls.Add(this.LoxCbox);
+            this.BagelGBox.Location = new System.Drawing.Point(261, 18);
+            this.BagelGBox.Margin = new System.Windows.Forms.Padding(5);
+            this.BagelGBox.Name = "BagelGBox";
+            this.BagelGBox.Padding = new System.Windows.Forms.Padding(5);
+            this.BagelGBox.Size = new System.Drawing.Size(160, 94);
+            this.BagelGBox.TabIndex = 1;
+            this.BagelGBox.TabStop = false;
+            this.BagelGBox.Text = "BAGEL";
+            this.BagelGBox.Visible = false;
+            // 
+            // CCCbox
+            // 
+            this.CCCbox.AutoSize = true;
+            this.CCCbox.Location = new System.Drawing.Point(8, 57);
+            this.CCCbox.Name = "CCCbox";
+            this.CCCbox.Size = new System.Drawing.Size(146, 24);
+            this.CCCbox.TabIndex = 4;
+            this.CCCbox.Text = "Cream Cheese";
+            this.CCCbox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CCCbox.UseVisualStyleBackColor = true;
+            this.CCCbox.CheckedChanged += new System.EventHandler(this.CCCbox_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(22, 149);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(70, 24);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Soda";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(22, 94);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(74, 24);
+            this.checkBox5.TabIndex = 2;
+            this.checkBox5.Text = "Salad";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // LoxCbox
+            // 
+            this.LoxCbox.AutoSize = true;
+            this.LoxCbox.Location = new System.Drawing.Point(8, 27);
+            this.LoxCbox.Name = "LoxCbox";
+            this.LoxCbox.Size = new System.Drawing.Size(56, 24);
+            this.LoxCbox.TabIndex = 1;
+            this.LoxCbox.Text = "Lox";
+            this.LoxCbox.UseVisualStyleBackColor = true;
+            this.LoxCbox.CheckedChanged += new System.EventHandler(this.LoxCbox_CheckedChanged);
+            // 
+            // SaladGBox
+            // 
+            this.SaladGBox.Controls.Add(this.ChefRadBtn);
+            this.SaladGBox.Controls.Add(this.SpinRadBtn);
+            this.SaladGBox.Controls.Add(this.checkBox9);
+            this.SaladGBox.Controls.Add(this.checkBox10);
+            this.SaladGBox.Location = new System.Drawing.Point(261, 122);
+            this.SaladGBox.Margin = new System.Windows.Forms.Padding(5);
+            this.SaladGBox.Name = "SaladGBox";
+            this.SaladGBox.Padding = new System.Windows.Forms.Padding(5);
+            this.SaladGBox.Size = new System.Drawing.Size(160, 94);
+            this.SaladGBox.TabIndex = 2;
+            this.SaladGBox.TabStop = false;
+            this.SaladGBox.Text = "SALAD";
+            this.SaladGBox.Visible = false;
+            // 
+            // ChefRadBtn
+            // 
+            this.ChefRadBtn.AutoSize = true;
+            this.ChefRadBtn.Location = new System.Drawing.Point(8, 57);
+            this.ChefRadBtn.Name = "ChefRadBtn";
+            this.ChefRadBtn.Size = new System.Drawing.Size(65, 24);
+            this.ChefRadBtn.TabIndex = 5;
+            this.ChefRadBtn.TabStop = true;
+            this.ChefRadBtn.Text = "Chef";
+            this.ChefRadBtn.UseVisualStyleBackColor = true;
+            this.ChefRadBtn.CheckedChanged += new System.EventHandler(this.ChefRadBtn_CheckedChanged);
+            // 
+            // SpinRadBtn
+            // 
+            this.SpinRadBtn.AutoSize = true;
+            this.SpinRadBtn.Location = new System.Drawing.Point(8, 27);
+            this.SpinRadBtn.Name = "SpinRadBtn";
+            this.SpinRadBtn.Size = new System.Drawing.Size(92, 24);
+            this.SpinRadBtn.TabIndex = 4;
+            this.SpinRadBtn.TabStop = true;
+            this.SpinRadBtn.Text = "Spinach";
+            this.SpinRadBtn.UseVisualStyleBackColor = true;
+            this.SpinRadBtn.CheckedChanged += new System.EventHandler(this.SpinRadBtn_CheckedChanged);
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(22, 149);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(70, 24);
+            this.checkBox9.TabIndex = 3;
+            this.checkBox9.Text = "Soda";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(22, 94);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(74, 24);
+            this.checkBox10.TabIndex = 2;
+            this.checkBox10.Text = "Salad";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // SodaGBox
+            // 
+            this.SodaGBox.Controls.Add(this.CSRadBtn);
+            this.SodaGBox.Controls.Add(this.ColaRadBtn);
+            this.SodaGBox.Controls.Add(this.checkBox8);
+            this.SodaGBox.Controls.Add(this.checkBox11);
+            this.SodaGBox.Location = new System.Drawing.Point(261, 226);
+            this.SodaGBox.Margin = new System.Windows.Forms.Padding(5);
+            this.SodaGBox.Name = "SodaGBox";
+            this.SodaGBox.Padding = new System.Windows.Forms.Padding(5);
+            this.SodaGBox.Size = new System.Drawing.Size(160, 94);
+            this.SodaGBox.TabIndex = 3;
+            this.SodaGBox.TabStop = false;
+            this.SodaGBox.Text = "SODA";
+            this.SodaGBox.Visible = false;
+            // 
+            // CSRadBtn
+            // 
+            this.CSRadBtn.AutoSize = true;
+            this.CSRadBtn.Location = new System.Drawing.Point(8, 57);
+            this.CSRadBtn.Name = "CSRadBtn";
+            this.CSRadBtn.Size = new System.Drawing.Size(126, 24);
+            this.CSRadBtn.TabIndex = 5;
+            this.CSRadBtn.TabStop = true;
+            this.CSRadBtn.Text = "Cream Soda";
+            this.CSRadBtn.UseVisualStyleBackColor = true;
+            this.CSRadBtn.CheckedChanged += new System.EventHandler(this.CSRadBtn_CheckedChanged);
+            // 
+            // ColaRadBtn
+            // 
+            this.ColaRadBtn.AutoSize = true;
+            this.ColaRadBtn.Location = new System.Drawing.Point(8, 27);
+            this.ColaRadBtn.Name = "ColaRadBtn";
+            this.ColaRadBtn.Size = new System.Drawing.Size(63, 24);
+            this.ColaRadBtn.TabIndex = 4;
+            this.ColaRadBtn.TabStop = true;
+            this.ColaRadBtn.Text = "Cola";
+            this.ColaRadBtn.UseVisualStyleBackColor = true;
+            this.ColaRadBtn.CheckedChanged += new System.EventHandler(this.ColaRadBtn_CheckedChanged);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(22, 149);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(70, 24);
+            this.checkBox8.TabIndex = 3;
+            this.checkBox8.Text = "Soda";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(22, 94);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(74, 24);
+            this.checkBox11.TabIndex = 2;
+            this.checkBox11.Text = "Salad";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(499, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PRICE";
+            // 
+            // bagelPrice
+            // 
+            this.bagelPrice.AutoSize = true;
+            this.bagelPrice.Location = new System.Drawing.Point(499, 61);
+            this.bagelPrice.Name = "bagelPrice";
+            this.bagelPrice.Size = new System.Drawing.Size(19, 20);
+            this.bagelPrice.TabIndex = 5;
+            this.bagelPrice.Text = "0";
+            // 
+            // saladPrice
+            // 
+            this.saladPrice.AutoSize = true;
+            this.saladPrice.Location = new System.Drawing.Point(499, 153);
+            this.saladPrice.Name = "saladPrice";
+            this.saladPrice.Size = new System.Drawing.Size(19, 20);
+            this.saladPrice.TabIndex = 6;
+            this.saladPrice.Text = "0";
+            // 
+            // sodaPrice
+            // 
+            this.sodaPrice.AutoSize = true;
+            this.sodaPrice.Location = new System.Drawing.Point(499, 255);
+            this.sodaPrice.Name = "sodaPrice";
+            this.sodaPrice.Size = new System.Drawing.Size(19, 20);
+            this.sodaPrice.TabIndex = 7;
+            this.sodaPrice.Text = "0";
+            // 
+            // TotalBtn
+            // 
+            this.TotalBtn.BackColor = System.Drawing.Color.Lime;
+            this.TotalBtn.Location = new System.Drawing.Point(227, 346);
+            this.TotalBtn.Name = "TotalBtn";
+            this.TotalBtn.Size = new System.Drawing.Size(75, 36);
+            this.TotalBtn.TabIndex = 8;
+            this.TotalBtn.Text = "TOTAL";
+            this.TotalBtn.UseVisualStyleBackColor = false;
+            this.TotalBtn.Click += new System.EventHandler(this.TotalBtn_Click);
+            // 
+            // totalPrice
+            // 
+            this.totalPrice.AutoSize = true;
+            this.totalPrice.Location = new System.Drawing.Point(499, 354);
+            this.totalPrice.Name = "totalPrice";
+            this.totalPrice.Size = new System.Drawing.Size(44, 20);
+            this.totalPrice.TabIndex = 9;
+            this.totalPrice.Text = "0.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(391, 354);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Total Price";
+            // 
+            // BakeryBagelForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 394);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.totalPrice);
+            this.Controls.Add(this.TotalBtn);
+            this.Controls.Add(this.sodaPrice);
+            this.Controls.Add(this.saladPrice);
+            this.Controls.Add(this.bagelPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SodaGBox);
+            this.Controls.Add(this.SaladGBox);
+            this.Controls.Add(this.BagelGBox);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "BakeryBagelForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bakery Menu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.BagelGBox.ResumeLayout(false);
+            this.BagelGBox.PerformLayout();
+            this.SaladGBox.ResumeLayout(false);
+            this.SaladGBox.PerformLayout();
+            this.SodaGBox.ResumeLayout(false);
+            this.SodaGBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox SodaCBox;
+        private System.Windows.Forms.CheckBox SalCbox;
+        private System.Windows.Forms.CheckBox BagCbox;
+        private System.Windows.Forms.GroupBox BagelGBox;
+        private System.Windows.Forms.CheckBox CCCbox;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox LoxCbox;
+        private System.Windows.Forms.GroupBox SaladGBox;
+        private System.Windows.Forms.RadioButton ChefRadBtn;
+        private System.Windows.Forms.RadioButton SpinRadBtn;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.GroupBox SodaGBox;
+        private System.Windows.Forms.RadioButton CSRadBtn;
+        private System.Windows.Forms.RadioButton ColaRadBtn;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label bagelPrice;
+        private System.Windows.Forms.Label saladPrice;
+        private System.Windows.Forms.Label sodaPrice;
+        private System.Windows.Forms.Button TotalBtn;
+        private System.Windows.Forms.Label totalPrice;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Bagel Menu/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Bagel_Menu.Bagel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Bagel_Menu
+{
+    public partial class BakeryBagelForm : Form
+    {
+        public BakeryBagelForm()
+        {
+            InitializeComponent();
+            SpinRadBtn.Checked = false;
+            ChefRadBtn.Checked = false;
+            ColaRadBtn.Checked = false;
+            CSRadBtn.Checked = false;
+        }
+
+        private double defBagelPrice = 2.25;
+        private double loxPrice = 1.20;
+        private double ccPrice = 1.00;
+        private double spinachPrice = 2.75;
+        private double chefPrice = 3.25;
+        private double colaPrice = 1.25;
+        private double creamSodaPrice = 1.25;
+        private double totalBagel = 0.00;
+        private double total = 0.00;
+
+        private void TotalBtn_Click(object sender, EventArgs e)
+        {
+            double bagelTotal = Convert.ToDouble(bagelPrice.Text);
+            double saladTotal = Convert.ToDouble(saladPrice.Text);
+            double sodaTotal = Convert.ToDouble(sodaPrice.Text);
+
+            total = bagelTotal + saladTotal + sodaTotal;
+            totalPrice.Text = total.ToString("0.##");
+
+            ClearSelection();
+        }
+
+
+        private void BagCbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BagCbox.Checked)
+            {
+                BagelGBox.Visible = true;
+                totalBagel += defBagelPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+            }
+            else
+            {
+                BagelGBox.Visible = false;
+                totalBagel -= defBagelPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+            }
+        }
+
+        private void SalCbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SalCbox.Checked)
+            {
+                SaladGBox.Visible = true;
+            }
+            else
+            {
+                SaladGBox.Visible = false;
+                saladPrice.Text = "0";
+                SpinRadBtn.Checked = false;
+                ChefRadBtn.Checked = false;
+            }
+        }
+        private void SodaCBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SodaCBox.Checked)
+            {
+                SodaGBox.Visible = true;
+            }
+            else
+            {
+                SodaGBox.Visible = false;
+                sodaPrice.Text = "0";
+                ColaRadBtn.Checked = false;
+                CSRadBtn.Checked = false;
+
+            }
+        }
+        private void LoxCbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (LoxCbox.Checked)
+            {
+                totalBagel += loxPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+            }
+            else
+            {
+                totalBagel -= loxPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+
+            }
+
+        }
+        private void CCCbox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CCCbox.Checked)
+            {
+                totalBagel += ccPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+            }
+            else
+            {
+                totalBagel -= ccPrice;
+                bagelPrice.Text = totalBagel.ToString("0.##");
+
+            }
+        }
+
+        private void SpinRadBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (SpinRadBtn.Checked)
+                saladPrice.Text = spinachPrice.ToString("0.##");
+        }
+
+        private void ChefRadBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ChefRadBtn.Checked)
+                saladPrice.Text = chefPrice.ToString("0.##");
+        }
+
+        private void ColaRadBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ColaRadBtn.Checked)
+                sodaPrice.Text = colaPrice.ToString("0.##");
+        }
+
+        private void CSRadBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CSRadBtn.Checked)
+                sodaPrice.Text = creamSodaPrice.ToString("0.##");
+        }
+
+        private void ClearSelection()
+        {
+            BagCbox.Checked = false;
+            SalCbox.Checked = false;
+            SodaCBox.Checked = false;
+            SpinRadBtn.Checked = false;
+            ChefRadBtn.Checked = false;
+            ColaRadBtn.Checked = false;
+            CSRadBtn.Checked = false;
+            bagelPrice.Text = "0";
+            saladPrice.Text = "0";
+            sodaPrice.Text = "0";
+
+        }
+
+    }
+}
