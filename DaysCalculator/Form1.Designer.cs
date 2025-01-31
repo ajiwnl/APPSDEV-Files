eadffd2a3c16b697d1e78ddede7dc4b05aefa6ec
Alvin John <92667829+ajiwnl@users.noreply.github.com>
2025-01-31 17:04:24 +0800

    Add files via upload

---
+++ DaysCalculator/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DaysCalculator
+{
+    partial class DaysCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CalculateBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YrInputTxtbox = new System.Windows.Forms.TextBox();
+            this.DaysLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CalculateBtn
+            // 
+            this.CalculateBtn.BackColor = System.Drawing.Color.LawnGreen;
+            this.CalculateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CalculateBtn.Location = new System.Drawing.Point(186, 174);
+            this.CalculateBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.CalculateBtn.Name = "CalculateBtn";
+            this.CalculateBtn.Size = new System.Drawing.Size(138, 47);
+            this.CalculateBtn.TabIndex = 0;
+            this.CalculateBtn.Text = "Calculate";
+            this.CalculateBtn.UseVisualStyleBackColor = false;
+            this.CalculateBtn.Click += new System.EventHandler(this.CalculateBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input Year/s:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Days in Year/s:";
+            // 
+            // YrInputTxtbox
+            // 
+            this.YrInputTxtbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YrInputTxtbox.Location = new System.Drawing.Point(237, 58);
+            this.YrInputTxtbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.YrInputTxtbox.Name = "YrInputTxtbox";
+            this.YrInputTxtbox.Size = new System.Drawing.Size(144, 29);
+            this.YrInputTxtbox.TabIndex = 3;
+            // 
+            // DaysLbl
+            // 
+            this.DaysLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DaysLbl.Location = new System.Drawing.Point(234, 100);
+            this.DaysLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DaysLbl.Name = "DaysLbl";
+            this.DaysLbl.Size = new System.Drawing.Size(147, 23);
+            this.DaysLbl.TabIndex = 4;
+            // 
+            // DaysCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(513, 242);
+            this.Controls.Add(this.DaysLbl);
+            this.Controls.Add(this.YrInputTxtbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CalculateBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(529, 281);
+            this.Name = "DaysCalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Years to Day Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalculateBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox YrInputTxtbox;
+        private System.Windows.Forms.Label DaysLbl;
+    }
+}
+
