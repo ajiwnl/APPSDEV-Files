cbb967099881eb5c587579501ee97c38d777dcad
Alvin John <92667829+ajiwnl@users.noreply.github.com>
2025-01-31 17:17:02 +0800

    Add files via upload

---
+++ OOP-Property/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Property
+{
+    internal class Student
+    {
+        int id;
+        string name;
+
+        public Student() { 
+        
+        }
+        public string Name
+        {
+            set
+            {
+                if ((value == string.Empty))
+                {
+                    name = "No Name";
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+            get { return name; }
+
+        }
+        public int IdNum
+        {
+            set
+            {
+
+                if ((value <= 0))
+                {
+                    id = 0;
+                }
+                else
+                {
+                    id = value;
+                }
+            }
+            get { return id; }
+
+        }
+        public new string ToString() //use the keyword new or override
+        {
+            return "ID No: " + id + Environment.NewLine + "Name: " + name;
+        }
+
+    }
+}
